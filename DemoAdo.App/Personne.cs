aa9827a8f098a5abc621f4b3dc24ed04c8ceedcc
Thierry Morre <thierry.morre@cognitic.be>
2025-06-03 15:09:34 +0200

    Add Personne ckass

---
+++ DemoAdo.App/Personne.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoAdo.App
+{
+    public class Personne
+    {
+        public int Id { get; }
+        public string Nom { get; }
+        public string Prenom { get; }
+        public string Email { get; }
+
+        public Personne(int id, string nom, string prenom, string email)
+        {
+            Id = id;
+            Nom = nom;
+            Prenom = prenom;
+            Email = email;
+        }
+    }
+}
--- DemoAdo.App/Program.cs
+++ DemoAdo.App/Program.cs
@@ -1,23 +1,70 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 // 1. On va jouter à notre projet Microsoft.Data.SqlClient (via Nuget)
+using DemoAdo.App;
 using Microsoft.Data.SqlClient;
 // 2. Etablir la connexion
 
-using (SqlConnection connection = new SqlConnection())
-{
-    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
-    connection.Open();
-    //Console.WriteLine(connection.State);
+//using (SqlConnection connection = new SqlConnection())
+//{
+//    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
+//    connection.Open();
+//    //Console.WriteLine(connection.State);
 
-    using (SqlCommand command = connection.CreateCommand())
-    { 
-        command.CommandText = "SELECT COUNT(*) FROM Personne;";
+//    using (SqlCommand command = connection.CreateCommand())
+//    { 
+//        command.CommandText = "SELECT COUNT(*) FROM Personne;";
 
-        int count = (int)command.ExecuteScalar();
-        Console.WriteLine($"Nombre de ligne(s) dans ma table Personne : {count}");
-    }
-}
+//        int count = (int)command.ExecuteScalar();
+//        Console.WriteLine($"Nombre de ligne(s) dans ma table Personne : {count}");
+//    }
+//}
+
+//using (SqlConnection connection = new SqlConnection())
+//{
+//    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
+//    connection.Open();
+//    //Console.WriteLine(connection.State);
+
+//    using (SqlCommand command = connection.CreateCommand())
+//    {
+//        command.CommandText = "SELECT GETDATE();";
+
+//        DateTime now = (DateTime)command.ExecuteScalar();
+//        Console.WriteLine($"Date : {now}");
+//    }
+//}
+
+//using (SqlConnection connection = new SqlConnection())
+//{
+//    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
+//    connection.Open();
+//    //Console.WriteLine(connection.State);
+
+//    using (SqlCommand command = connection.CreateCommand())
+//    {
+//        command.CommandText = "SELECT 'Personne' AS Type, Id, Nom, Prenom, Email FROM Personne;";
+
+//        List<Personne> personnes = new List<Personne>();
+//        using (SqlDataReader reader = command.ExecuteReader())
+//        {
+//            while (reader.Read())
+//            {
+//                personnes.Add(new Personne((int)reader["Id"], (string)reader["Nom"], (string)reader["Prenom"], (string)reader["Email"]));
+//            }
+//        }
+
+//        foreach (Personne personne in personnes)
+//        {
+//            Console.WriteLine($"{personne.Id} : {personne.Nom} {personne.Prenom} ({personne.Email})");
+//        }
+//    }
+//}
+
+string nom = "Willis";
+string prenom = "Bruce";
+string email = "bruce4.willis@imdb.com'); use master; drop database demoAdo; --";
+
 
 using (SqlConnection connection = new SqlConnection())
 {
@@ -27,30 +74,9 @@ using (SqlConnection connection = new SqlConnection())
 
     using (SqlCommand command = connection.CreateCommand())
     {
-        command.CommandText = "SELECT GETDATE();";
+        command.CommandText = $"INSERT INTO Personne (Nom, Prenom, Email) VALUES (N'{nom}', N'{prenom}', N'{email}')";
 
-        DateTime now = (DateTime)command.ExecuteScalar();
-        Console.WriteLine($"Date : {now}");
-    }
-}
-
-using (SqlConnection connection = new SqlConnection())
-{
-    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
-    connection.Open();
-    //Console.WriteLine(connection.State);
-
-    using (SqlCommand command = connection.CreateCommand())
-    {
-        command.CommandText = "SELECT 'Personne' AS Type, Id, Nom, Prenom, Email FROM Personne;";
-
-        using (SqlDataReader reader = command.ExecuteReader())
-        {
-
-            while (reader.Read())
-            {
-                Console.WriteLine(reader["Email"]);
-            }
-        }
+        int rows = command.ExecuteNonQuery();
+        Console.WriteLine($"Nombre de ligne(s) modifiée : {rows}");
     }
 }
