c7faf3a7997dfa98fc9e6fc88da9b8b4863cac33
Thierry Morre <thierry.morre@cognitic.be>
2025-06-04 16:50:45 +0200

    Création de la classe de connexion

--- DemoAdo.App/Program.cs
+++ DemoAdo.App/Program.cs
@@ -3,149 +3,172 @@
 // 1. On va jouter à notre projet Microsoft.Data.SqlClient (via Nuget)
 using DemoAdo.App;
 using Microsoft.Data.SqlClient;
-using System.Data;
-using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Data.Common;
+using BStorm.Tools.Database;
 // 2. Etablir la connexion
 
-//using (SqlConnection connection = new SqlConnection())
-//{
-//    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
-//    connection.Open();
-//    //Console.WriteLine(connection.State);
-
-//    using (SqlCommand command = connection.CreateCommand())
-//    { 
-//        command.CommandText = "SELECT COUNT(*) FROM Personne;";
-
-//        int count = (int)command.ExecuteScalar();
-//        Console.WriteLine($"Nombre de ligne(s) dans ma table Personne : {count}");
-//    }
-//}
-
-//using (SqlConnection connection = new SqlConnection())
-//{
-//    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
-//    connection.Open();
-//    //Console.WriteLine(connection.State);
-
-//    using (SqlCommand command = connection.CreateCommand())
-//    {
-//        command.CommandText = "SELECT GETDATE();";
-
-//        DateTime now = (DateTime)command.ExecuteScalar();
-//        Console.WriteLine($"Date : {now}");
-//    }
-//}
-
-//using (SqlConnection connection = new SqlConnection())
-//{
-//    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
-//    connection.Open();
-//    //Console.WriteLine(connection.State);
-
-//    using (SqlCommand command = connection.CreateCommand())
-//    {
-//        command.CommandText = "SELECT 'Personne' AS Type, Id, Nom, Prenom, Email, Tel FROM Personne;";
-
-//        List<Personne> personnes = new List<Personne>();
-//        using (SqlDataReader reader = command.ExecuteReader())
-//        {
-//            while (reader.Read())
-//            {
-//                personnes.Add(new Personne((int)reader["Id"], (string)reader["Nom"], (string)reader["Prenom"], (string)reader["Email"], reader["Tel"] as string));
-//            }
-//        }
-
-//        foreach (Personne personne in personnes)
-//        {
-//            Console.WriteLine($"{personne.Id} : {personne.Nom} {personne.Prenom} (@:{personne.Email} - Tel:{personne.Tel ?? "aucun"}) ");
-//        }
-//    }
-//}
-
-string nom = "Doe";
-string prenom = "Micahel";
-string email = "michael.doe@test.be";
-string? tel = "+32 (0)477 00 00 00";
-
-
-//using (SqlConnection connection = new SqlConnection())
-//{
-//    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
-//    connection.Open();
-//    //Console.WriteLine(connection.State);
-
-//    using (SqlCommand command = connection.CreateCommand())
-//    {
-//        command.CommandText = $"INSERT INTO Personne (Nom, Prenom, Email) VALUES (@Nom, @Prenom, @Email);";
-//        Console.WriteLine(command.CommandText);
-//        command.Parameters.AddWithValue("nom", nom);
-//        command.Parameters.AddWithValue("prenom", prenom);
-//        command.Parameters.AddWithValue("email", email);
-
-//        int rows = command.ExecuteNonQuery();
-//        Console.WriteLine($"Nombre de ligne(s) modifiée : {rows}");
-//    }
-//}
-
-//using (SqlConnection connection = new SqlConnection())
-//{
-//    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
-//    connection.Open();
-//    //Console.WriteLine(connection.State);
-
-//    using (SqlCommand command = connection.CreateCommand())
-//    {
-//        command.CommandText = $"INSERT INTO Personne (Nom, Prenom, Email, Tel) OUTPUT inserted.Id VALUES (@Nom, @Prenom, @Email, @Tel);";
-//        Console.WriteLine(command.CommandText);
-//        command.Parameters.AddWithValue("nom", nom);
-//        command.Parameters.AddWithValue("prenom", prenom);
-//        command.Parameters.AddWithValue("email", email);
-//        command.Parameters.AddWithValue("tel", tel ?? (object)DBNull.Value);
-
-//        int id = (int)command.ExecuteScalar();
-//        Console.WriteLine($"l'id de {prenom} {nom} est {id}");
-//    }
-//}
-
-
-//using (SqlConnection connection = new SqlConnection())
-//{
-//    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
-//    connection.Open();
-//    //Console.WriteLine(connection.State);
-
-//    using (SqlCommand command = connection.CreateCommand())
-//    {
-//        command.CommandText = $"INSERT INTO Personne (Nom, Prenom, Email, Tel) OUTPUT inserted.Id VALUES (@Nom, @Prenom, @Email, @Tel);";
-//        Console.WriteLine(command.CommandText);
-//        command.Parameters.AddWithValue("nom", nom);
-//        command.Parameters.AddWithValue("prenom", prenom);
-//        command.Parameters.AddWithValue("email", email);
-//        command.Parameters.AddWithValue("tel", tel ?? (object)DBNull.Value);
-
-//        int id = (int)command.ExecuteScalar();
-//        Console.WriteLine($"l'id de {prenom} {nom} est {id}");
-//    }
-//}
-
-using (SqlConnection connection = new SqlConnection())
+using (SqlConnection connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;"))
 {
-    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
     connection.Open();
-    //Console.WriteLine(connection.State);
+    int result = (int)connection.ExecuteScalar("SELECT COUNT(*) FROM Personne;");
+    Console.WriteLine($"Nombre de ligne(s) dans ma table Personne : {result}");
 
-    using (SqlCommand command = connection.CreateCommand())
+    //using (SqlConnection connection = new SqlConnection())
+    //{
+    //    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
+    //    connection.Open();
+    //    //Console.WriteLine(connection.State);
+
+    //    using (SqlCommand command = connection.CreateCommand())
+    //    { 
+    //        command.CommandText = "SELECT COUNT(*) FROM Personne;";
+
+    //        int count = (int)command.ExecuteScalar();
+    //        Console.WriteLine($"Nombre de ligne(s) dans ma table Personne : {count}");
+    //    }
+    //}
+
+    DateTime now = (DateTime)connection.ExecuteScalar("SELECT GETDATE();");
+    Console.WriteLine($"Date : {now}");
+
+    //using (SqlConnection connection = new SqlConnection())
+    //{
+    //    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
+    //    connection.Open();
+    //    //Console.WriteLine(connection.State);
+
+    //    using (SqlCommand command = connection.CreateCommand())
+    //    {
+    //        command.CommandText = "SELECT GETDATE();";
+
+    //        DateTime now = (DateTime)command.ExecuteScalar();
+    //        Console.WriteLine($"Date : {now}");
+    //    }
+    //}
+
+    //using (SqlConnection connection = new SqlConnection())
+    //{
+    //    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
+    //    connection.Open();
+    //    //Console.WriteLine(connection.State);
+
+    //    using (SqlCommand command = connection.CreateCommand())
+    //    {
+    //        command.CommandText = "SELECT 'Personne' AS Type, Id, Nom, Prenom, Email, Tel FROM Personne;";
+
+    //        List<Personne> personnes = new List<Personne>();
+    //        using (SqlDataReader reader = command.ExecuteReader())
+    //        {
+    //            while (reader.Read())
+    //            {
+    //                personnes.Add(new Personne((int)reader["Id"], (string)reader["Nom"], (string)reader["Prenom"], (string)reader["Email"], reader["Tel"] as string));
+    //            }
+    //        }
+
+    //        foreach (Personne personne in personnes)
+    //        {
+    //            Console.WriteLine($"{personne.Id} : {personne.Nom} {personne.Prenom} (@:{personne.Email} - Tel:{personne.Tel ?? "aucun"}) ");
+    //        }
+    //    }
+    //}
+
+    IEnumerable<Personne> personnes = connection.ExecuteReader("SELECT 'Personne' AS Type, Id, Nom, Prenom, Email, Tel FROM Personne;", dr => new Personne((int)dr["Id"], (string)dr["Nom"], (string)dr["Prenom"], (string)dr["Email"], dr["Tel"] as string));
+
+    foreach (Personne personne in personnes)
     {
-        command.CommandText = $"[dbo].[CSP_AjoutPersonne]";
-        command.CommandType = CommandType.StoredProcedure;
-        Console.WriteLine(command.CommandText);
-        command.Parameters.AddWithValue("Nom", nom);
-        command.Parameters.AddWithValue("Prenom", prenom);
-        command.Parameters.AddWithValue("Email", email);
-        command.Parameters.AddWithValue("Tel", tel ?? (object)DBNull.Value);
-
-        int rows = command.ExecuteNonQuery();
-        Console.WriteLine($"Nombre de ligne(s) modifiée: {rows}");
+        Console.WriteLine($"{personne.Id} : {personne.Nom} {personne.Prenom} (@:{personne.Email} - Tel:{personne.Tel ?? "aucun"}) ");
     }
+
+
+    string nom = "Doe";
+    string prenom = "Micahel";
+    string email = "bidule.doe@test.be";
+    string? tel = "+32 (0)477 00 00 00";
+
+
+    //using (SqlConnection connection = new SqlConnection())
+    //{
+    //    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
+    //    connection.Open();
+    //    //Console.WriteLine(connection.State);
+
+    //    using (SqlCommand command = connection.CreateCommand())
+    //    {
+    //        command.CommandText = $"INSERT INTO Personne (Nom, Prenom, Email) VALUES (@Nom, @Prenom, @Email);";
+    //        Console.WriteLine(command.CommandText);
+    //        command.Parameters.AddWithValue("nom", nom);
+    //        command.Parameters.AddWithValue("prenom", prenom);
+    //        command.Parameters.AddWithValue("email", email);
+
+    //        int rows = command.ExecuteNonQuery();
+    //        Console.WriteLine($"Nombre de ligne(s) modifiée : {rows}");
+    //    }
+    //}
+
+    //using (SqlConnection connection = new SqlConnection())
+    //{
+    //    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
+    //    connection.Open();
+    //    //Console.WriteLine(connection.State);
+
+    //    using (SqlCommand command = connection.CreateCommand())
+    //    {
+    //        command.CommandText = $"INSERT INTO Personne (Nom, Prenom, Email, Tel) OUTPUT inserted.Id VALUES (@Nom, @Prenom, @Email, @Tel);";
+    //        Console.WriteLine(command.CommandText);
+    //        command.Parameters.AddWithValue("nom", nom);
+    //        command.Parameters.AddWithValue("prenom", prenom);
+    //        command.Parameters.AddWithValue("email", email);
+    //        command.Parameters.AddWithValue("tel", tel ?? (object)DBNull.Value);
+
+    //        int id = (int)command.ExecuteScalar();
+    //        Console.WriteLine($"l'id de {prenom} {nom} est {id}");
+    //    }
+    //}
+
+
+    //using (SqlConnection connection = new SqlConnection())
+    //{
+    //    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
+    //    connection.Open();
+    //    //Console.WriteLine(connection.State);
+
+    //    using (SqlCommand command = connection.CreateCommand())
+    //    {
+    //        command.CommandText = $"INSERT INTO Personne (Nom, Prenom, Email, Tel) OUTPUT inserted.Id VALUES (@Nom, @Prenom, @Email, @Tel);";
+    //        Console.WriteLine(command.CommandText);
+    //        command.Parameters.AddWithValue("nom", nom);
+    //        command.Parameters.AddWithValue("prenom", prenom);
+    //        command.Parameters.AddWithValue("email", email);
+    //        command.Parameters.AddWithValue("tel", tel ?? (object)DBNull.Value);
+
+    //        int id = (int)command.ExecuteScalar();
+    //        Console.WriteLine($"l'id de {prenom} {nom} est {id}");
+    //    }
+    //}
+
+    //using (SqlConnection connection = new SqlConnection())
+    //{
+    //    connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoAdo;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;";
+    //    connection.Open();
+    //    //Console.WriteLine(connection.State);
+
+    //    using (SqlCommand command = connection.CreateCommand())
+    //    {
+    //        command.CommandText = $"[dbo].[CSP_AjoutPersonne]";
+    //        command.CommandType = CommandType.StoredProcedure;
+    //        Console.WriteLine(command.CommandText);
+    //        command.Parameters.AddWithValue("Nom", nom);
+    //        command.Parameters.AddWithValue("Prenom", prenom);
+    //        command.Parameters.AddWithValue("Email", email);
+    //        command.Parameters.AddWithValue("Tel", tel ?? (object)DBNull.Value);
+
+    //        int rows = command.ExecuteNonQuery();
+    //        Console.WriteLine($"Nombre de ligne(s) modifiée: {rows}");
+    //    }
+    //}
+
+
+    //Console.WriteLine(connection.State);
+    int rows = connection.ExecuteNonQuery("[dbo].[CSP_AjoutPersonne]", true, new { Nom = "Test", Prenom = "Test", Email = "test3@test.be" });
+    Console.WriteLine($"Nombre de ligne(s) modifiée: {rows}");
 }
---
+++ Tools.Database/DbConnectionExtensions.cs
@@ -0,0 +1,65 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Reflection;
+
+namespace Tools.Database
+{
+    public static class DbConnectionExtensions
+    {
+        public static object? ExecuteScalar(this DbConnection dbConnection, string query, bool isStoredProcedure = false, object? parameters = null)
+        {
+            using (DbCommand dbCommand = CreateCommand(dbConnection, query, isStoredProcedure, parameters))
+            {                
+                object? result = dbCommand.ExecuteScalar();
+                return result is DBNull ? null : result;
+            }
+        }
+
+        public static int ExecuteNonQuery(this DbConnection dbConnection, string query, bool isStoredProcedure = false, object? parameters = null)
+        {
+            using (DbCommand dbCommand = CreateCommand(dbConnection, query, isStoredProcedure, parameters))
+            {
+                return dbCommand.ExecuteNonQuery();
+            }
+        }
+
+        public static IEnumerable<T> ExecuteReader<T>(this DbConnection dbConnection, string query, Func<IDataRecord, T> selector, bool isStoredProcedure = false, object? parameters = null)
+        {
+            using (DbCommand dbCommand = CreateCommand(dbConnection, query, isStoredProcedure, parameters))
+            {
+                using (DbDataReader reader = dbCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        yield return selector(reader);
+                    }
+                }
+            }
+        }
+
+        private static DbCommand CreateCommand(DbConnection connection, string query, bool isStoredProcedure, object? parameters)
+        {
+            DbCommand dbCommand = connection.CreateCommand();
+            dbCommand.CommandText = query;
+            if (isStoredProcedure)
+                dbCommand.CommandType = CommandType.StoredProcedure;
+
+            if(parameters is not null)
+            {
+                Type parameterType = parameters.GetType();
+
+                IEnumerable<PropertyInfo> propertyInfos = parameterType.GetProperties().Where(p => p.CanRead); ;
+                foreach (PropertyInfo propertyInfo in propertyInfos)
+                {
+                    DbParameter dbParameter = dbCommand.CreateParameter();
+                    dbParameter.ParameterName = propertyInfo.Name;
+                    dbParameter.Value = propertyInfo.GetValue(parameters) ?? DBNull.Value;
+                    dbCommand.Parameters.Add(dbParameter);
+                }
+            }
+
+
+            return dbCommand;
+        }
+    }
+}
